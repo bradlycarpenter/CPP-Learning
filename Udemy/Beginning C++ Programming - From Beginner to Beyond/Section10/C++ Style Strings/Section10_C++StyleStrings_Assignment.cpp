96f93d378ae1c42e4a1df970ea3d13e06e227831
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-08-09 11:08:02 +0200

    Update Section10_C++StyleStrings_Assignment.cpp

--- Udemy/Beginning C++ Programming - From Beginner to Beyond/Section10/C++ Style Strings/Section10_C++StyleStrings_Assignment.cpp
+++ Udemy/Beginning C++ Programming - From Beginner to Beyond/Section10/C++ Style Strings/Section10_C++StyleStrings_Assignment.cpp
@@ -3,16 +3,23 @@
 
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 int main(){
-    string user_string{};
-    cout << "Please enter the word you would like to see displayed as a pyramid: " << endl;
-    getline(cin,user_string);
-    string pyra_half{""};
-    for (size_t i{0};i<user_string.length();++i){
-        pyra_half += user_string[i];
-        cout << pyra_half << endl;
+    string user{"123456"};
+    string output{""};
+    string reverse_output{""};
+    
+    //cout << "Please enter the word you would like to see displayed as a pyramid: " << endl;
+    //getline(cin,user);
+   for (size_t i = 0; i < user.length(); ++i) {
+        string white_spaces(user.length() - output.length(),' ');
+        cout << white_spaces << output + reverse_output << white_spaces << endl;
+        reverse_output = "";
+        for (size_t j = output.length(); j > 0; --j) {
+            reverse_output += output[j - 1];
+        }
+        output += user[i];
     }
+    cout << output + reverse_output << endl;
 }
