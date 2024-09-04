e0867976c67256bc3dd4b845b562d0d600554f4d
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-09-04 19:09:58 +0200

    Update Section11_RecursiveFunctions_Fibonacci.cpp

--- Udemy/Beginning C++ Programming - From Beginner to Beyond/Section11/Recursive Functions/Section11_RecursiveFunctions_Fibonacci.cpp
+++ Udemy/Beginning C++ Programming - From Beginner to Beyond/Section11/Recursive Functions/Section11_RecursiveFunctions_Fibonacci.cpp
@@ -7,11 +7,12 @@ unsigned long long fibonacci (unsigned long long n){
 	if (n <= 1){
 		return n;        // base cases
 	}
+	cout << n << endl;
 	return fibonacci(n-1) + fibonacci(n-2); // recursion
 }
 
 int main(){
-	cout << fibonacci(5) << endl; // 5
-	cout << fibonacci(30) << endl; // 832040
-	cout << fibonacci(40) << endl; // 102334155
+	cout << fibonacci(30) << endl; // 5
+	// cout << fibonacci(30) << endl; // 832040
+	// cout << fibonacci(40) << endl; // 102334155
 }
