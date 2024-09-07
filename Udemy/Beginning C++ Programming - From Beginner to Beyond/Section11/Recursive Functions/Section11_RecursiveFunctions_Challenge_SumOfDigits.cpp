91b0b18e8fa9e824b7063f064f29da16dcd01618
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-09-07 02:15:24 +0200

    modified/Section11_RecursiveFunctions_Challenge_SumOfDigits

--- Udemy/Beginning C++ Programming - From Beginner to Beyond/Section11/Recursive Functions/Section11_RecursiveFunctions_Challenge_SumOfDigits.cpp
+++ Udemy/Beginning C++ Programming - From Beginner to Beyond/Section11/Recursive Functions/Section11_RecursiveFunctions_Challenge_SumOfDigits.cpp
@@ -3,22 +3,29 @@
 
 using namespace std;
 
-int sum_of_digits(int n);
+int iterative_sum_of_digits(int n);
+int recursive_sum_of_digits(int n);
 
-int sum_of_digits(int n){
+int iterative_sum_of_digits(int n){
     string numbers = to_string(n);
     size_t total{0};
     for (char c:numbers){
-        cout << c << endl;
-        total += c;
-        cout << total << endl;
+        total += c - 48;
     }
     return total;
+}
+
+int recursive_sum_of_digits(int n){
+    if (n == 0){
+        return 0;
+    }
+    return (n % 10 + recursive_sum_of_digits(n /10));
 }
 
 int main(){
     cout << "Enter a number: ";
     int number{};
     cin >> number;
-    cout << sum_of_digits(number) << endl;
+    cout << "Iterative Sum: " << iterative_sum_of_digits(number) << endl;
+    cout << "Relative Sum: " << recursive_sum_of_digits(number) << endl;
 }
