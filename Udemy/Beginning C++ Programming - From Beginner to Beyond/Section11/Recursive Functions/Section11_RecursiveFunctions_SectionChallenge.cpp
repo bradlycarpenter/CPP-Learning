bbb5aaf51eee74af53c4ea06b89430d3dba4bd8c
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-09-08 02:29:45 +0200

    update/cooked-the-challenge

--- Udemy/Beginning C++ Programming - From Beginner to Beyond/Section11/Recursive Functions/Section11_RecursiveFunctions_SectionChallenge.cpp
+++ Udemy/Beginning C++ Programming - From Beginner to Beyond/Section11/Recursive Functions/Section11_RecursiveFunctions_SectionChallenge.cpp
@@ -1,100 +1,127 @@
-// Section 9
+// Section 11
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-void display_menu();
-void print_numbers(vector<int> numbers);
+const string print_options();
+string print_numbers(const vector<int> &num);
+inline void add_numbers(vector<int> &num, char &opt);
+double mean_number(const vector<int> &num);
+int smallest_number(const vector<int> &num);
+int largest_number(const vector<int> &num);
 
-void display_menu(){
-    cout << "-------------------------------" << endl;
-    cout << "P - Print numbers" << endl;
-    cout << "A - Add a number" << endl;
-    cout << "M - Display mean of the numbers" << endl;
-    cout << "S - Display the smallest number" << endl;
-    cout << "L - Display the largest number" << endl;
-    cout << "Q - Quit" << endl;
-    cout << "-------------------------------" << endl;
-}
-
-void print_numbers(vector<int> numbers){
-    cout << "-------------------------------" << endl;
-    cout << "[ ";
-    if (numbers.empty()) {
-        cout << "[] - No numbers to display." << endl;
-    } else {
-        for (auto i : numbers) cout << i << " ";
-    }
-    cout << "]" << endl;
-}
-
-int main() {
-    char opt{};
+int main(){
     vector<int> numbers{};
-    int num_to_add{};
-    int total{};
-    int smallest{};
-    int largest{};
+    char opt{};
 
     do {
-        display_menu();
+        cout << print_options();
         cin >> opt;
+
         switch (tolower(opt)) {
             case 'p': {
-                print_numbers(numbers);
+                cout << print_numbers(numbers);
                 break;
             }
             case 'a': {
-                do {
-                    cout << "What number would you like to add?: " << endl;
-                    cin >> num_to_add;
-                    numbers.push_back(num_to_add);
-                    cout << "Would you like to add another number? (Y/N)"
-                         << endl;
-                    cin >> opt;
-                } while (opt != 'n' && opt != 'N');
+                add_numbers(numbers,opt);
                 break;
             }
             case 'm': {
-                if (numbers.empty()) {
-                    cout << "No numbers to median." << endl;
+                if (numbers.empty()){
+                    cout << "No numbers to mean\n";
                 } else {
-                    for (auto i : numbers) total += i;
-                    cout << "The average is: "
-                         << static_cast<double>(total) / numbers.size() << endl;
+                    cout << "The average is: " + to_string(mean_number(numbers)) + "\n";
                 }
                 break;
             }
             case 's': {
                 if (numbers.empty()) {
-                    cout << "No numbers to display smallest." << endl;
+                    cout << "No numbers to display smallest.\n";
                 } else {
-                    smallest = numbers.at(0);
-                    for (auto i : numbers) {
-                        if (smallest > i) smallest = i;
-                    }
-                    cout << "The smallest number is: " << smallest << endl;
+                    cout << "The smallest number is: " 
+                        << to_string(smallest_number(numbers)) + "\n";
                 }
                 break;
             }
             case 'l': {
                 if (numbers.empty()) {
-                    cout << "No numbers to display largest." << endl;
+                    cout << "No numbers to display largest.\n";
                 } else {
-                    largest = numbers.at(0);
-                    for (auto i : numbers) {
-                        if (largest < i) largest = i;
-                    }
-                    cout << "The largest number is: " << largest << endl;
+                    cout << "The largest number is: " 
+                        << to_string(largest_number(numbers)) + "\n";
                 }
                 break;
             }
             case 'q':
-                cout << "Goodbye" << endl;
+                cout << "Goodbye\n";
                 break;
             default:
                 cout << "Unknown selection, please try again";
         }
     } while (opt != 'Q' && opt != 'q');
+}
+
+const string print_options(){
+    const string options{
+        "-------------------------------\n"
+        "P - Print numbers\n"
+        "A - Add a number\n"
+        "M - Display mean of the numbers\n"
+        "S - Display the smallest number\n"
+        "L - Display the largest number\n"
+        "Q - Quit\n"
+    };
+    return options;
+}
+
+string print_numbers(const vector<int> &num){
+    string numbers{};
+    if (num.empty()){
+        numbers = "[] - No numbers to display.";
+    } else {
+        for (auto i:num){
+            numbers += to_string(i) + " ";
+        }
+    }
+    return numbers + "\n";
+}
+
+inline void add_numbers(vector<int> &num, char &opt){
+    int num_to_add{};
+        do {
+            cout << "What number would you like to add?: \n";
+            cin >> num_to_add;
+            num.push_back(num_to_add);
+            cout << "Would you like to add another number? (Y/N)\n";
+            cin >> opt;
+        } while (opt != 'n' && opt != 'N');
+}
+
+double mean_number(const vector<int> &num){
+    int total{};
+    for (auto i:num) total += i;
+    return static_cast<double>(total) / num.size();
+}
+
+int smallest_number(const vector<int> &num){
+    int smallest = num.at(0);
+    for (auto i:num){
+        if (smallest > i){
+            smallest = i;
+        }
+    }
+    return smallest;
+}
+
+int largest_number(const vector<int> &num){
+    int largest = num.at(0);
+    for (auto i:num){
+        if (largest < i){
+            largest = i;
+        }
+    }
+    return largest;
 }
