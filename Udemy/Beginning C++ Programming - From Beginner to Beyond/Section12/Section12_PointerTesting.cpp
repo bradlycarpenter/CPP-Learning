0bd95296fe9057f7b8d49a5639d96e3f847261ce
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-09-13 19:07:02 +0200

    delete

--- Udemy/Beginning C++ Programming - From Beginner to Beyond/Section12/Section12_PointerTesting.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    int x{4};
-    int *pX = &x;
-    cout << "pX is equal to: " << pX << endl;
-    cout << "x is equal to: " << &x << endl;
-}
