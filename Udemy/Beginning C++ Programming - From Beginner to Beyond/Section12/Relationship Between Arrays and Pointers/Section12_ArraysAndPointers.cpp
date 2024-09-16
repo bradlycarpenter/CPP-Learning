69f3ad6870bc8227afe7e4b3f2773acd8ffd43e9
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-09-17 01:09:46 +0200

    update/clang-format

--- Udemy/Beginning C++ Programming - From Beginner to Beyond/Section12/Relationship Between Arrays and Pointers/Section12_ArraysAndPointers.cpp
+++ Udemy/Beginning C++ Programming - From Beginner to Beyond/Section12/Relationship Between Arrays and Pointers/Section12_ArraysAndPointers.cpp
@@ -1,33 +1,30 @@
 #include <iostream>
 
-int main(){
-    int scores[]{100, 95, 89};
-    std::cout << "Value of scores: " << scores << std::endl;
+int main() {
+  int scores[]{100, 95, 89};
+  std::cout << "Value of scores: " << scores << std::endl;
 
-    int *score_ptr{scores};
-    std::cout << "Value of score_ptr: " << score_ptr << std::endl;
+  int *score_ptr{scores};
+  std::cout << "Value of score_ptr: " << score_ptr << std::endl;
 
-    std::cout << "\nArray of subscript notation -------------------" 
-        << std::endl;
-    std::cout << scores[0] << std::endl;
-    std::cout << scores[1] << std::endl;
-    std::cout << scores[2] << std::endl;
+  std::cout << "\nArray of subscript notation -------------------" << std::endl;
+  std::cout << scores[0] << std::endl;
+  std::cout << scores[1] << std::endl;
 
-    std::cout << "\nPointer subscript notation -------------------" 
-        << std::endl;
-    std::cout << score_ptr[0] << std::endl;
-    std::cout << score_ptr[1] << std::endl;
-    std::cout << score_ptr[2] << std::endl;
+  std::cout << scores[2] << std::endl;
 
-    std::cout << "\nPointer offset notation -------------------" 
-        << std::endl;
-    std::cout << *score_ptr << std::endl;
-    std::cout << *(score_ptr + 1) << std::endl;
-    std::cout << *(score_ptr + 2) << std::endl;
+  std::cout << "\nPointer subscript notation -------------------" << std::endl;
+  std::cout << score_ptr[0] << std::endl;
+  std::cout << score_ptr[1] << std::endl;
+  std::cout << score_ptr[2] << std::endl;
 
-    std::cout << "\nArray offset notation -------------------" 
-        << std::endl;
-    std::cout << *scores << std::endl;
-    std::cout << *(scores + 1) << std::endl;
-    std::cout << *(scores + 2) << std::endl;
+  std::cout << "\nPointer offset notation -------------------" << std::endl;
+  std::cout << *score_ptr << std::endl;
+  std::cout << *(score_ptr + 1) << std::endl;
+  std::cout << *(score_ptr + 2) << std::endl;
+
+  std::cout << "\nArray offset notation -------------------" << std::endl;
+  std::cout << *scores << std::endl;
+  std::cout << *(scores + 1) << std::endl;
+  std::cout << *(scores + 2) << std::endl;
 }
