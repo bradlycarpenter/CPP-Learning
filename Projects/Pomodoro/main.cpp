921e827b203f71092124a5ed58c509d43bde9955
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-09-21 20:14:38 +0200

    update/timer-function

--- Projects/Pomodoro/main.cpp
+++ Projects/Pomodoro/main.cpp
@@ -1,6 +1,4 @@
 #include <chrono>
-#include <cstdio>
-#include <ctime>
 #include <iostream>
 #include <thread>
 
@@ -10,36 +8,33 @@ using namespace std;
 void Timer(size_t &seconds);
 
 int main() {
-  cout << "Starting up Pomodoro Tracker";
-  cout << "\nHow long would you like your first split to be? (Minutes): ";
+  system("clear");
+
+  cout << "Starting up Pomodoro Tracker"
+       << "\nHow long would you like your first split to be? (Minutes): ";
+
   size_t minutes;
   cin >> minutes;
   size_t seconds = minutes * 60;
+
   Timer(seconds);
 }
 
 void Timer(size_t &seconds) {
+  system("clear");
+  // Red
   cout << "\x1b[91m";
+
   for (size_t i{seconds}; i > 0; --i) {
     if (i > 119) {
-      cout << i / 60 << " Minutes remaining";
+      cout << i / 60 << " Minutes remaining\n";
     } else if (i > 59) {
-      cout << i / 60 << " Minute remaining";
+      cout << i / 60 << " Minute remaining\n";
     } else {
-      cout << i << " Seconds remaining";
+      cout << i << " Seconds remaining\n";
     }
-    cout << "\n";
-    this_thread::sleep_for(chrono::seconds(1)); // Freeze for 1 second
-    cout << "\x1B[1A";
-    cout << "\x1B[0K";
-    }
-}
 
-// string getPresentDateTime() {
-//   // Declare a time_t variable to hold the current time.
-//   time_t tt;
-//   // Declare a pointer to a tm struct to hold the local time.
-//   struct tm *st;
-// 
-//   return asctime(st);
-// }
+    this_thread::sleep_for(chrono::seconds(1)); // Freeze for 1 second
+    system("clear");
+  }
+}
