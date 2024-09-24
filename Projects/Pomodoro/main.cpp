db9a4b1422fa9323474f8e9baf51cb69ffb0736e
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-09-25 00:50:06 +0200

    update/formatting

--- Projects/Pomodoro/main.cpp
+++ Projects/Pomodoro/main.cpp
@@ -2,48 +2,47 @@
 #include <iostream>
 #include <thread>
 
-using namespace std;
-
 void clearScreen();
 void timer(size_t seconds);
 
 int main() {
   clearScreen();
 
-  cout << "--------------------------------------------------------\n"
-          "-              Welcome to the Pomodoro Timer           -\n"
-          "--------------------------------------------------------\n";
+  std::cout << "--------------------------------------------------------\n"
+               "-              Welcome to the Pomodoro Timer           -\n"
+               "--------------------------------------------------------\n";
 
-  cout << "\nHow long would you like your working splits to be? (Minutes): ";
+  std::cout
+      << "\nHow long would you like your working splits to be? (Minutes): ";
   size_t minutes;
-  cin >> minutes;
+  std::cin >> minutes;
   size_t split_seconds = minutes * 60;
-  cout << "\nHow long would you like your breaks to be? (Minutes): ";
-  cin >> minutes;
+  std::cout << "\nHow long would you like your breaks to be? (Minutes): ";
+  std::cin >> minutes;
   size_t break_seconds = minutes * 60;
 
   char working{'Y'};
   clearScreen();
   do {
-    cout << "Working..\n";
+    std::cout << "Working..\n";
     timer(split_seconds);
 
-    cout << "Work Split is Over\n";
-    this_thread::sleep_for(chrono::seconds(1)); // Freeze for 1 second
+    std::cout << "Work Split is Over\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1)); // Freeze for 1 second
     clearScreen();
 
-    cout << "Break Time!\n";
+    std::cout << "Break Time!\n";
     timer(break_seconds);
 
-    this_thread::sleep_for(chrono::seconds(1)); // Freeze for 1 second
+    std::this_thread::sleep_for(std::chrono::seconds(1)); // Freeze for 1 second
     clearScreen();
 
-    cout << "Would you like to continue working? ('y' = Yes, 'n' = No)\n";
-    cin >> working;
+    std::cout << "Would you like to continue working? ('y' = Yes, 'n' = No)\n";
+    std::cin >> working;
     clearScreen();
   } while (toupper(working) == 'Y');
 
-  cout << "See you later";
+  std::cout << "See you later";
 }
 
 void clearScreen() {
@@ -61,34 +60,35 @@ void timer(size_t seconds) {
 
   for (size_t i{seconds}; i > 0; --i) {
     if (i > 119) {
-      cout << i / 60 << " Minutes remaining\n";
+      std::cout << i / 60 << " Minutes remaining\n";
     } else if (i > 59) {
-      cout << i / 60 << " Minute remaining\n";
+      std::cout << i / 60 << " Minute remaining\n";
     } else {
-      cout << i << " Seconds remaining\n";
+      std::cout << i << " Seconds remaining\n";
     }
-    cout << "[";
+    std::cout << "[";
 
     for (size_t j{0}; j < PROGRESS_BAR_LENGTH * progress_percent_reverse; ++j) {
-      cout << "+";
+      std::cout << "+";
     }
 
     for (size_t j{0}; j < PROGRESS_BAR_LENGTH * progress_percent; ++j) {
-      cout << "-";
+      std::cout << "-";
     }
 
     progress_percent = static_cast<double>(i) / static_cast<double>(seconds);
     progress_percent_reverse = 1 - progress_percent;
 
-    cout << "]";
+    std::cout << "]";
 
-    cout << "\n";
+    std::cout << "\n";
 
-    this_thread::sleep_for(chrono::milliseconds(50)); // Freeze for 1 second
+    std::this_thread::sleep_for(
+        std::chrono::milliseconds(50)); // Freeze for 1 second
 
-    cout << "\x1B[1A"; // Move cursor up one line
-    cout << "\x1B[0K"; // Delete from cursor till end of line
-    cout << "\x1B[1A"; // Move cursor up one line
-    cout << "\x1B[0K"; // Delete from cursor till end of line
+    std::cout << "\x1B[1A"; // Move cursor up one line
+    std::cout << "\x1B[0K"; // Delete from cursor till end of line
+    std::cout << "\x1B[1A"; // Move cursor up one line
+    std::cout << "\x1B[0K"; // Delete from cursor till end of line
   }
 }
