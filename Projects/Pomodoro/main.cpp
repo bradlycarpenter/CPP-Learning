23bfb315dee2a1dd4769789051718487f7d08d4f
Bradly Carpenter <bradlycarpenterza@gmail.com>
2024-10-01 02:09:53 +0200

    update/namespace-is-kinda-good

--- Projects/Pomodoro/main.cpp
+++ Projects/Pomodoro/main.cpp
@@ -1,54 +1,61 @@
 #include <chrono>
 #include <cmath>
+#include <ctime>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <thread>
 
+using namespace std;
+
 void clearScreen();
-void timer(std::size_t seconds);
+void timer(size_t seconds);
 
 int main() {
   clearScreen();
 
-  std::cout << "--------------------------------------------------------\n"
-               "-              Welcome to the Pomodoro Timer           -\n"
-               "--------------------------------------------------------\n";
+  auto current_time = chrono::system_clock::now();
+  time_t time_now = chrono::system_clock::to_time_t(current_time);
+  cout << ctime(&time_now);
 
-  std::cout
-      << "\nHow long would you like your working splits to be? (Minutes): ";
-  std::size_t minutes;
-  std::cin >> minutes;
-  std::size_t split_seconds = minutes * 60;
-  std::cout << "\nHow long would you like your breaks to be? (Minutes): ";
-  std::cin >> minutes;
-  std::size_t break_seconds = minutes * 60;
+  cout << "--------------------------------------------------------\n"
+          "-              Welcome to the Pomodoro Timer           -\n"
+          "--------------------------------------------------------\n";
+
+  cout << "\nHow long would you like your working splits to be? (Minutes): ";
+  size_t minutes;
+  cin >> minutes;
+  size_t split_seconds = minutes * 60;
+  cout << "\nHow long would you like your breaks to be? (Minutes): ";
+  cin >> minutes;
+  size_t break_seconds = minutes * 60;
 
   char working{'Y'};
   clearScreen();
   do {
-    std::cout << "Working..\n";
+    cout << "Working..\n";
     timer(split_seconds);
 
-    std::cout << "Work Split is Over\n";
-    std::this_thread::sleep_for(std::chrono::seconds(1)); // Freeze for 1 second
+    cout << "Work Split is Over\n";
+    this_thread::sleep_for(chrono::seconds(1)); // Freeze for 1 second
     clearScreen();
 
-    std::cout << "Are you ready to start your break? ('y' = Yes)\n";
-    std::cin >> working;
+    cout << "Are you ready to start your break? ('y' = Yes)\n";
+    cin >> working;
     clearScreen();
 
-    std::cout << "Break Time!\n";
+    cout << "Break Time!\n";
     timer(break_seconds);
 
-    std::this_thread::sleep_for(std::chrono::seconds(1)); // Freeze for 1 second
+    this_thread::sleep_for(chrono::seconds(1)); // Freeze for 1 second
     clearScreen();
 
-    std::cout << "Would you like to continue working? ('y' = Yes, 'n' = No)\n";
-    std::cin >> working;
+    cout << "Would you like to continue working? ('y' = Yes, 'n' = No)\n";
+    cin >> working;
     clearScreen();
   } while (toupper(working) == 'Y');
 
-  std::cout << "See you later";
+  cout << "See you later";
 }
 
 void clearScreen() {
@@ -59,24 +66,23 @@ void clearScreen() {
 #endif
 }
 
-void timer(std::size_t seconds) {
+void timer(size_t seconds) {
   const int PROGRESS_BAR_LENGTH{60};
 
-  for (std::size_t i{seconds}; i > 0; --i) {
+  for (size_t i{seconds}; i > 0; --i) {
     if (i >= 60) {
-      std::cout << i / 60 << " Minute" << (i > 119 ? "s" : "")
-                << " remaining\n";
+      cout << i / 60 << " Minute" << (i > 119 ? "s" : "") << " remaining\n";
     } else {
-      std::cout << i << " Second" << (i > 1 ? "s" : "") << " remaining\n";
+      cout << i << " Second" << (i > 1 ? "s" : "") << " remaining\n";
     }
 
     double progress_percent = static_cast<double>(seconds - i) / seconds;
-    std::size_t plus_char_amount = round(PROGRESS_BAR_LENGTH * progress_percent);
-    std::size_t minus_char_amount = PROGRESS_BAR_LENGTH - plus_char_amount;
-    std::cout << "[" << std::string(plus_char_amount, '+')
-              << std::string(minus_char_amount, '-') << "]\n";
+    size_t plus_char_amount = round(PROGRESS_BAR_LENGTH * progress_percent);
+    size_t minus_char_amount = PROGRESS_BAR_LENGTH - plus_char_amount;
+    cout << "[" << string(plus_char_amount, '+')
+         << string(minus_char_amount, '-') << "]\n";
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout << "\x1B[1A\x1B[0K\x1B[1A\x1B[0K";
+    this_thread::sleep_for(chrono::seconds(1));
+    cout << "\x1B[1A\x1B[0K\x1B[1A\x1B[0K";
   }
 }
